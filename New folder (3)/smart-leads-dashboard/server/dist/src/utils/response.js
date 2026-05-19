9a08761aef70f7c6afa78b5a1de6d4d3e2637a7f
Rakshitha L U <rakshur1512@gmail.com>
2026-05-19 23:54:12 +0530

    Delete New folder (3) directory

--- New folder (3)/smart-leads-dashboard/server/dist/src/utils/response.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendResponse = void 0;
-const sendResponse = (res, status, success, message, data) => {
-    res.status(status).json({ success, message, data });
-};
-exports.sendResponse = sendResponse;
--- New folder (3)/smart-leads-dashboard/server/src/utils/csv.ts
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const { Parser } = require("json2csv");
-
-export const generateCSV = (data: any[]) => {
-  const parser = new Parser();
-  return parser.parse(data);
-};
